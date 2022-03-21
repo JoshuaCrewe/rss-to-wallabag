a90ba85062fd5ccfef5b10850cca8367a1a0c84a
Joshua <joshua@venncreative.co.uk>
2022-03-21 16:35:53 +0000

    Authenticate sending data to wallabag
    The auth only lasts an hour so each time this script is run it needs to
    get an access token and pass that along with the request. This is a
    working example of this process. It could be cleaned up.
    
    @TODO:
    - Add comments to code
    - Reduce cruft
    - Store sensitive information not in a yaml plain text
    - Store current access token in case run more than once in an hour

--- bag.go
+++ bag.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 	"github.com/mmcdole/gofeed"
 	"github.com/spf13/viper"
+    "io"
+    "log"
 	"net/http"
+    // "net/http/httputil"
 	"net/url"
 )
 
@@ -30,7 +33,127 @@ type feeds struct {
 	Feeds []feedItem
 }
 
+type BagConfig struct {
+    BaseUrl string `mapstructure:"baseUrl"`
+    AccessToken string `mapstructure:"access_token"`
+    ClientID string `mapstructure:"client_id"`
+    ClientSecret string `mapstructure:"client_secret"`
+    UserName string `mapstructure:"username"`
+    Password string `mapstructure:"password"`
+}
+
+type Response struct {
+    AccessToken string `json:"access_token"`
+    RefreshToken string `json:"refresh_token"`
+}
+
+// @TODO Delete this when done
+func debug(data []byte, err error) {
+    if err == nil {
+        fmt.Printf("%s\n\n", data)
+    } else {
+        log.Fatalf("%s\n\n", err)
+    }
+}
+
+
+func auth() string {
+	type PostRequest struct {
+        GrantType string `json:"grant_type"`
+        ClientID string `json:"client_id"`
+        ClientSecret string `json:"client_secret"`
+        UserName string `json:"username"`
+        Password string `json:"password"`
+	}
+
+
+	Config := BagConfig{}
+	err := viper.Unmarshal(&Config)
+
+	// Check for errors
+	if err != nil {
+		// Print the error to the console
+		panic(err)
+	}
+    bearer := "Bearer " + Config.AccessToken
+
+	// Pocket API enpoint for adding items
+	baseURL := Config.BaseUrl + "api/entries.json"
+
+	// Configure a new request using the URL and Json
+	req, err := http.NewRequest("GET", baseURL, nil)
+	req.Header.Set("Content-Type", "application/json")
+    req.Header.Add("Authorization", bearer)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+    // Check for errors
+    if err != nil {
+        // Print the error to the console
+        panic(err)
+    }
+
+    // fmt.Println("🍺  this is run ", resp.Status)
+    // fmt.Println("🍺  this is run ", resp)
+	// Close the response
+	defer resp.Body.Close()
+
+    if resp.StatusCode != 200 {
+        // fmt.Println("🍺  Do Auth ", resp.StatusCode)
+        // Pocket API enpoint for adding items
+        baseURL := Config.BaseUrl + "oauth/v2/token"
+
+        jsonStr := &PostRequest{
+            "password",
+            Config.ClientID,
+            Config.ClientSecret,
+            Config.UserName,
+            Config.Password,
+        }
+
+        // fmt.Println("🍺  jsonStr ", jsonStr)
+
+        // Json encode this data
+        b, err := json.Marshal(jsonStr)
+
+        // fmt.Println("🍺  jsonStr ", bytes.NewBuffer(b))
+        // Configure a new request using the URL and Json
+        req, err := http.NewRequest("POST", baseURL, bytes.NewBuffer(b))
+        req.Header.Add("Content-Type", "application/json")
+        req.Header.Add("Accept", "application/json, */*;q=0.5")
+        req.Header.Add("Authorization", bearer)
+
+        // debug(httputil.DumpRequestOut(req, true))
+
+        client := &http.Client{}
+        resp, err := client.Do(req)
+        fmt.Println("🍺  after auth ", resp.Body)
+
+        // Check for errors
+        if err != nil {
+            // Print the error to the console
+            panic(err)
+        }
+        // Close the response
+        defer resp.Body.Close()
+
+
+        fmt.Println("🍺  The Response ", resp.Body)
+
+        b, err = io.ReadAll(resp.Body)
+        if err != nil {
+            log.Fatalln(err)
+        }
+
+        return string(b)
+    }
+    return ""
+}
+
 func main() {
+    viper.SetConfigType("yaml")
+
 	// Set the name for the config file
 	viper.SetConfigName("bag")
 
@@ -40,6 +163,7 @@ func main() {
 	// Set another path
 	viper.AddConfigPath("$HOME/.config/bag")
 
+
 	// Read what the config says
 	err := viper.ReadInConfig()
 
@@ -48,6 +172,35 @@ func main() {
 		// Print the error to the console
 		panic(err)
 	}
+
+    auth := auth()
+    var response Response
+
+    err = json.Unmarshal([]byte(auth), &response)
+
+    fmt.Println("🍺  response ", response.AccessToken)
+
+	// Get the pocket specific data
+	Config := BagConfig{}
+
+	// Populate struct with config data
+	err = viper.Unmarshal(&Config)
+
+	// Check for errors
+	if err != nil {
+		// Print the error to the console
+		panic(err)
+	}
+
+    // Config.AccessToken = response.AccessToken
+
+    // Update the configuration file
+	// viper.Set("response.AccessToken", Config.AccessToken)
+
+    // fmt.Println("Config after auth", Config)
+	// viper.AddConfigPath("$HOME/.config/bag")
+    // viper.SafeWriteConfig()
+    // return
 
 	// Create a new struct for feeds
 	f := feeds{}
@@ -86,13 +239,16 @@ func main() {
 		for _, element := range feed.Items {
 
 			// If the current ID is the last one we got
-			if element.GUID == latest {
+			if element.Link == latest {
 				// Stop looking through the posts
 				break
 			} else {
 				// Send all the newer posts to wallabag
+				// fmt.Println("Element:", (element))
 				// fmt.Println("send to Bag:", element.Link)
-				send(element.Link, tags)
+				// fmt.Println("Current:", element.GUID)
+				// fmt.Println("send to Bag:", tags)
+				send(element.Link, tags, response.AccessToken)
 			}
 		}
 		// Add a space for prettyness
@@ -100,7 +256,7 @@ func main() {
 
 		// For the current feed update the latest post for the first url
 		// received from the rss parser
-		f.Feeds[i].LatestPost = feed.Items[0].GUID
+		f.Feeds[i].LatestPost = feed.Items[0].Link
 
 		// Increase the count
 		i++
@@ -120,12 +276,7 @@ func main() {
 	}
 }
 
-func send(postURL string, Tags string) {
-	// BagConfig : The Wallabag API will need a key and access token to work with
-	type BagConfig struct {
-        BaseUrl string `mapstructure:"baseUrl"`
-		AccessToken string `mapstructure:"access_token"`
-	}
+func send(postURL string, Tags string, AccessToken string) {
 
 	// PostRequest : To construct some json which can be sent to the Pocket API
 	type PostRequest struct {
@@ -160,7 +311,7 @@ func send(postURL string, Tags string) {
 	// Json encode this data
 	b, err := json.Marshal(jsonStr)
 
-    bearer := "Bearer " + Config.AccessToken
+    bearer := "Bearer " + AccessToken
 
 	// Configure a new request using the URL and Json
 	req, err := http.NewRequest("POST", baseURL, bytes.NewBuffer(b))
@@ -170,6 +321,9 @@ func send(postURL string, Tags string) {
 	// Make the HTTP request
 	client := &http.Client{}
 	resp, err := client.Do(req)
+
+	fmt.Println("🐛  ", req)
+	fmt.Println("🐛  ", resp)
 
 	// Handle errors
 	if err != nil {
